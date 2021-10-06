6a1cac9c7e091c7c6787c68b258b23ad37b37d7d
Josecarlos61 <carlosmelian2013@gmail.com>
2021-10-06 21:38:39 +0100

    web-con sass

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const menu = document.getElementById('menu');
+window.onscroll = function () {
+    if (window.pageYOffset > 10) {
+        menu.style.backgroundColor = '#fff';
+    } else {
+        menu.style.backgroundColor = 'initial';
+    }
+}
